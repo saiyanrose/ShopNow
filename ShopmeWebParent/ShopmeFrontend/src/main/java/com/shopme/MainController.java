af017d60de277bc76bb6261a99b94f6c762cce2c
sayianrose <njr58618@gmail.com>
2022-10-05 14:20:43 +0530

    login functionality frontend basic authentication

--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/MainController.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/MainController.java
@@ -3,6 +3,9 @@ package com.shopme;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +25,14 @@ public class MainController {
 		model.addAttribute("listCategory", category);
 		model.addAttribute("pageTitle","Shopme");
 		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String viewLoginPage() {
+		Authentication authentication=SecurityContextHolder.getContext().getAuthentication();		
+		if(authentication==null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+		return "redirect:/";
 	}
 }
